cf10267901d77793817be45864f9bd8fbc0c7af6
Raniel Sousa <worldtech15@hotmail.com>
2013-10-08 17:51:52 -0300

    Maquina Hipotetica

--- MH/LerArquivo.java
+++ MH/LerArquivo.java
@@ -1,51 +1,50 @@
+package br.com.mh.MaquinaEstado;
 
-package com.br.MH;
-// importação das classes
+
+import java.awt.Image;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
-import javax.swing.JOptionPane;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class LerArquivo {
-	//estaciação da classe File e Buff	
-	FileReader FileR;
-	BufferedReader BuffR;
-	// metodo contrutor 
-	public LerArquivo(){
-	
-		// execão de caracterios 
-		try {
-			//estaciação 
-			FileR = new FileReader("C:\\Users\\RANIEL\\workspace\\Maquina Hipotetica\\src\\com\\br\\MH\\Arquivos\\Arquivo Teste.txt");
-	        BuffR = new BufferedReader(FileR);
-	        // equnto estiver caracterios no aquivo ele vai ler 
-	        while( BuffR.ready()){
-	        	// imprimir o  arquivo.txt
-	        	
-	        	JOptionPane.showInternalMessageDialog(null, BuffR.readLine());
-	        }
-		  
-		 // feixamento do BuffR 
-		 BuffR.close();
-		 
-		 
-		 } 
-		 
-		 //exeção
-		 catch (FileNotFoundException e) {
+
+
+public static void main(String[] args)throws IOException , InterruptedException {
+	int Estado = 0;
+  
+	String Arquivo = "C:\\Users\\Wena Barreto\\workspace\\MaquinaHipotetica\\src\\br\\com\\mh\\MaquinaEstado\\Arquivo.txt";
+  InputStream is = new FileInputStream(Arquivo);
+  InputStreamReader isr = new InputStreamReader(is);
+  BufferedReader br = new BufferedReader(isr);
+  String imprimir = br.readLine();
+  
+  while(imprimir != null){
+	  char simbolo = ' ';
+			  
+	  for (int i = 0; i < imprimir.length(); i++) {
+		 simbolo = imprimir.charAt(i);
+		 System.out.print(simbolo +  "  ");
 		
-			
-		}
-		
-		///exeção 
-		catch(IOException ex){
-			
-		}
 		
 	}
+	
+ Estado = (Estado +1);
+ if (Estado == 4) {
+	 Estado = 0;
+	 Estado = (Estado +1);
+	
+}
+ 
+	  System.out.println(imprimir+ "  " + "Estado " + Estado );
+ Thread.sleep( 1 * 1000 );
+imprimir = br.readLine();
+
+	
 
 }
-
+}
+}
 
